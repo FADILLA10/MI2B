87566727d711f626d5aac983e41b97156d32b9ce
HP <HP@LAPTOP-N4VHDLDP>
2023-03-13 09:41:23 +0700

    First commit

---
+++ src/FADILLA_02032023/HELLOWORLD.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FADILLA_02032023;
+
+/**
+ *
+ * @author HP
+ */
+public class HELLOWORLD {
+    
+}
